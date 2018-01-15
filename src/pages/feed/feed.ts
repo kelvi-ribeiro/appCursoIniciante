4001f7dcc91012947914e72558b458489e450827
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.edu.br>
2018-01-15 17:14:01 -0200

    Sessao de Typescript terminada

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -15,11 +15,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class FeedPage {
 
+  public nomeUsuario:string = "Kelvi Martins Ribeiro"
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
+  public somaDoisNumeros(num1:number,num2:number):void{
+    alert(num1+num2);
+  }
+
   ionViewDidLoad() {
-    console.log('ionViewDidLoad FeedPage');
+    this.somaDoisNumeros(4,4);
   }
 
 }
